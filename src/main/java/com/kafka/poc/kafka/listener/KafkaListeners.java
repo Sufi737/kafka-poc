8a07f34fec330651e8e8d20e4a6dd03250627a0b
Sufyan <khotsufyan@gmail.com>
2023-01-02 12:16:59 +0530

    Initial commit

---
+++ src/main/java/com/kafka/poc/kafka/listener/KafkaListeners.java
@@ -0,0 +1,16 @@
+package com.kafka.poc.kafka.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(
+        topics = "demo",
+        groupId = "groupId"
+    )
+    void listener(String data) {
+        System.out.println("Listener received: "+data);
+    }
+}
